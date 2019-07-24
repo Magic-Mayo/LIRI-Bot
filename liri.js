adbb75aa09910c8784fe942eb6197adfd59ff0f3
Magic-Mayo <mike.mayoii@gmail.com>
2019-07-24 13:55:34 -0700

    made functions to search albums and tracks just like the function for artists. need to fix issue with only going thru one returned response before it stops

--- liri.js
+++ liri.js
@@ -57,9 +57,10 @@ spotifyIt = () => {
     ]).then(answer=>{
         const input = String(answer.choice.toLowerCase());
         
-        spotify.search({type: input, query: answer.name}).then(response => {
-            const artist = response.artists.items
+        spotify.search({type: input, query: '\"' + answer.name + '\"', limit: 40}).then(response => {
+            
             artistSearch = (count) => {
+                const artist = response.artists.items
                 if (artist.length > count){
                     console.log(
                         '\nArtist: ' + artist[count].name,
@@ -68,8 +69,8 @@ spotifyIt = () => {
                         '\nLink to Artist: ' + artist[count].external_urls.spotify,
                         '\n',
                         '\n--------------------------------------------------------------------------------'
-                    )
-                    artistCount++
+                    );
+                    artistCount++;
                 } else {
                     console.log('\n--------------------------------------------------------------------------------', '\n',
                     '\nSorry, we couldn\'t find your artist :(', '\n',
@@ -77,8 +78,46 @@ spotifyIt = () => {
                 }
             }
 
+            albumSearch = (count) => {
+                const album = response.albums.items
+                if (album.length > count){
+                    console.log(
+                        '\nArtist: ' + album[count].artists[0].name,
+                        '\nAlbum: ' + album[count].name,
+                        '\nTotal Tracks: ' + album[count].total_tracks,
+                        '\nAlbum Release: ' + moment(album[count].release_date).format('MM/DD/YYYY'),
+                        '\nLink to album: ' + album[count].external_urls.spotify,
+                        '\n', '\n--------------------------------------------------------------------------------'
+                    );
+                    albumCount++;
+                } else {
+                    console.log('\n--------------------------------------------------------------------------------', '\n',
+                    '\nSorry, we couldn\'t find your album :(', '\n',
+                    '\n--------------------------------------------------------------------------------')
+                }
+            }
+
+            trackSearch = (count) => {
+                const track = response.tracks.items;
+                if (track.length > count){
+                    console.log(
+                        '\nTrack: ' + answer.name,
+                        '\nArtist: ' + track[count].artists[0].name,
+                        '\nAlbum: ' + track[count].album.name,
+                        '\nAlbum Release Date: ' + track[count].album.release_date,
+                        '\nLink to track: ' + track[count].external_urls.spotify
+                    )
+                    trackCount++;
+                } else {
+                    console.log('\n--------------------------------------------------------------------------------', '\n',
+                    '\nSorry, we couldn\'t find your track :(', '\n',
+                    '\n--------------------------------------------------------------------------------')
+                }
+            }
+
             console.log('\n--------------------------------------------------------------------------------')
             if (input === 'artist'){
+                const artist = response.artists.items
                 spotifyArtist.push(artist);
                 artistSearch(artistCount);
                 inq.prompt([
@@ -90,26 +129,60 @@ spotifyIt = () => {
                     }
                 ]).then(answer => {
                     if (!answer.confirm){
-                        artistSearch();
+                        artistSearch(artistCount);
                     } else {
                         console.log('\n--------------------------------------------------------------------------------', '\n',
                         '\nSearch complete', '\n',
-                        '\n--------------------------------------------------------------------------------')
+                        '\n--------------------------------------------------------------------------------');
+                        artistCount = 0;
+                        spotifyArtist = [];
                     }
                 })
             } else if (input === 'album'){
                 const album = response.albums.items
-                // console.log(album[0])
-                console.log(
-                    '\nArtist: ' + album[0].artists[0].name,
-                    '\nAlbum: ' + album[0].name,
-                    '\nTotal Tracks: ' + album[0].total_tracks,
-                    '\nAlbum Release: ' + moment(album[0].release_date).format('MM/DD/YYYY'),
-                    '\nLink to album: ' + album[0].external_urls.spotify
-                )
+                spotifyAlbum.push(album);
+                albumSearch(albumCount);
+                inq.prompt([
+                    {
+                        type: 'confirm',
+                        message: 'Is this the album you were searching for?',
+                        name: 'confirm',
+                        default: true
+                    }
+                ]).then(answer => {
+                    if (!answer.confirm){
+                        albumSearch(albumCount);
+                    } else {
+                        console.log('\n--------------------------------------------------------------------------------', '\n',
+                        '\nSearch complete', '\n',
+                        '\n--------------------------------------------------------------------------------');
+                        albumCount = 0;
+                        spotifyAlbum = [];
+                    }
+                })
             } else if (input === 'track'){
                 const track = response.tracks.items
-                console.log(track[0])
+                console.log(track[0].artists[0].name)
+                spotifyTrack.push(track);
+                trackSearch(trackCount);
+                inq.prompt([
+                    {
+                        type: 'confirm',
+                        message: 'Is this the track you were searching for?',
+                        name: 'confirm',
+                        default: true
+                    }
+                ]).then(answer => {
+                    if (!answer.confirm){
+                        trackSearch(trackCount);
+                    } else {
+                        console.log('\n--------------------------------------------------------------------------------', '\n',
+                        '\nSearch complete', '\n',
+                        '\n--------------------------------------------------------------------------------');
+                        trackCount = 0;
+                        spotifyTrack = [];
+                    }
+                })
             }
 
         })
