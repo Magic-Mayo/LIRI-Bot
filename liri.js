f7652d8ce27fce9b088d0d8ba49353f745facbb6
Magic-Mayo <mike.mayoii@gmail.com>
2019-07-24 13:15:50 -0700

    added inquirer prompts to ask user if they want to search spotify, omdb, or concerts. made function to search spotify for track, artist or album depending on user input. made another function to return 20 results from an artist search and log info and confirm with user if that was the artist they wanted. if not will keep returning as many artist as were in the original response until either the user confirms the search or there are no more responses

--- liri.js
+++ liri.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 const fs = require('fs')
 const inq = require('inquirer');
-const mom = require('moment');
+const moment = require('moment');
 const keys = require('./keys');
 const axios = require('axios');
-const spotify = require('node-spotify-api');
-// const spotifyKey = new Spotify(keys.spotify);
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify(keys.spotify);
+let spotifyArtist = [];
+let spotifyAlbum = [];
+let spotifyTrack = [];
+let artistCount = 0;
+let albumCount = 0;
+let trackCount = 0;
 
 /* use moment to log the time and date of each request
 
@@ -19,29 +25,28 @@ userInput = () => {
     inq.prompt([
         {
             type: 'list',
-            message: 'What are you searching for?',
-            choices: ['Song', 'Movie', 'Concert'],
+            message: 'What would you like to do?',
+            choices: ['Spotify-It', 'OMDB-It', 'Concert-It'],
             name: 'choice'
         }
     ]).then(answer=>{
-        if (answer.choice === 'Song'){
+        if (answer.choice === 'Spotify-It'){
             spotifyIt();
         }
-        else if (answer.choice === 'Movie'){
+        else if (answer.choice === 'OMDB-It'){
             OMDBIt();
         }
-        else if (answer.choice === 'Concert'){
+        else if (answer.choice === 'Concert-It'){
             concertIt();
         }
     })
 }
 
 spotifyIt = () => {
-    console.log('\n----------------------------------------')
     inq.prompt([
         {
             type: 'list',
-            message: 'What do you want to search for?',
+            message: 'What do you want to Spotify?',
             choices: ['Track', 'Artist', 'Album'],
             name: 'choice'
         },{
@@ -50,20 +55,64 @@ spotifyIt = () => {
             name: 'name'
         }
     ]).then(answer=>{
-
-        const input = String(answer.choice);
+        const input = String(answer.choice.toLowerCase());
         
-        if (input === 'Song'){
-            axios.get('https://api.spotify.com/v1/search/q='+answer.name).then(response => {
-            console.log(
-                'Artist: ' + response
-            )
-            })
-        } else if (input === 'Album' || input === 'Artist'){
-            axios.get('https://api.spotify.com/v1/search/q='+answer.name+'type='+input).then(response => {
+        spotify.search({type: input, query: answer.name}).then(response => {
+            const artist = response.artists.items
+            artistSearch = (count) => {
+                if (artist.length > count){
+                    console.log(
+                        '\nArtist: ' + artist[count].name,
+                        '\nGenres: ' + artist[count].genres.join(', '),
+                        '\nSpotify Followers: ' + artist[count].followers.total,
+                        '\nLink to Artist: ' + artist[count].external_urls.spotify,
+                        '\n',
+                        '\n--------------------------------------------------------------------------------'
+                    )
+                    artistCount++
+                } else {
+                    console.log('\n--------------------------------------------------------------------------------', '\n',
+                    '\nSorry, we couldn\'t find your artist :(', '\n',
+                    '\n--------------------------------------------------------------------------------')
+                }
+            }
 
-            })
-        }
+            console.log('\n--------------------------------------------------------------------------------')
+            if (input === 'artist'){
+                spotifyArtist.push(artist);
+                artistSearch(artistCount);
+                inq.prompt([
+                    {
+                        type: 'confirm',
+                        message: 'Is this the artist you were searching for?',
+                        name: 'confirm',
+                        default: true
+                    }
+                ]).then(answer => {
+                    if (!answer.confirm){
+                        artistSearch();
+                    } else {
+                        console.log('\n--------------------------------------------------------------------------------', '\n',
+                        '\nSearch complete', '\n',
+                        '\n--------------------------------------------------------------------------------')
+                    }
+                })
+            } else if (input === 'album'){
+                const album = response.albums.items
+                // console.log(album[0])
+                console.log(
+                    '\nArtist: ' + album[0].artists[0].name,
+                    '\nAlbum: ' + album[0].name,
+                    '\nTotal Tracks: ' + album[0].total_tracks,
+                    '\nAlbum Release: ' + moment(album[0].release_date).format('MM/DD/YYYY'),
+                    '\nLink to album: ' + album[0].external_urls.spotify
+                )
+            } else if (input === 'track'){
+                const track = response.tracks.items
+                console.log(track[0])
+            }
+
+        })
     })
 }
 
